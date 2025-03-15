c7d3c540c7de081d0b1674385aed7cf39bfa4a04
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-15 15:49:44 +0530

    Added Navbar for the website

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import styles from "./App.module.css";
+import { Navbar } from "./components/Navbar/Navbar";
 
 function App() {
   return (
     <div className={styles.App}>
-      <h1>My Portfolio</h1>
+      <Navbar />
     </div>
   );
 }
