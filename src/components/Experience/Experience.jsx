855de2b8065ace2fdd22c4fa0f03c72e06ba31a1
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-16 21:18:36 +0530

    Added Projects section for the website

--- src/components/Experience/Experience.jsx
+++ src/components/Experience/Experience.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React from "react";
 
 import styles from "./Experience.module.css";
 import skills from "../../data/skills.json";
---
+++ src/components/Projects/Projects.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+import styles from "./Projects.module.css";
+import projects from "../../data/projects.json";
+import { Projectcard } from "./Projectcard";
+
+export const Projects = () => {
+  return (
+    <section className={styles.container} id="projects">
+      <h2 className={styles.title}>Projects</h2>
+      <div className={styles.projects}>
+        {projects.map((project, id) => {
+          return <Projectcard key={id} project={project} />;
+        })}
+      </div>
+    </section>
+  );
+};
