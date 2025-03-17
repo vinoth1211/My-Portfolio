8fcfb5a96a0bbb0c52c04eccd50788106306b565
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-17 19:15:36 +0530

    Configure React app for deployment to GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: "/rvinoth11-Portfolio",
   css: {
     modules: {
       localsConvention: "camelCase",
