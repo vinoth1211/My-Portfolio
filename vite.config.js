fc8a4b82a947c1df13f1d7547048de7e5dd62ee8
Vinoth <rvinoth@std.foc.sab.ac.lk>
2025-03-18 20:50:35 +0530

    Revert "Configure React app for deployment to GitHub Pages"
    This reverts commit 8fcfb5a96a0bbb0c52c04eccd50788106306b565.

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,6 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/rvinoth11-Portfolio",
   css: {
     modules: {
       localsConvention: "camelCase",
